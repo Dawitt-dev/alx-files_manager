0c61a9ba4e490f237b67c0eb28f1f1232bc777fa
Dawitt <126588225+Dawitt-dev@users.noreply.github.com>
2024-09-04 21:56:23 +0000

    improved for more accuracy

--- utils/redis.js
+++ utils/redis.js
@@ -5,16 +5,21 @@ class RedisClient {
     this.client = redis.createClient();
     this.connected = false;
 
-    this.connectPromise = new Promise((resolve, reject) => {
+    // Handle connection events
+    this.client.on('connect', () => {
+      console.log('Redis client connected to the server');
+      this.connected = true;
+    });
+
+    this.client.on('error', (error) => {
+      console.error(`Redis client not connected to the server: ${error.message}`);
+    });
+
+    // Ensure the client is connected before performing operations
+    this.ensureConnection = new Promise((resolve) => {
       this.client.on('connect', () => {
-        console.log('Redis client connected to the server');
         this.connected = true;
         resolve();
-      });
-
-      this.client.on('error', (error) => {
-        console.error(`Redis client not connected to the server: ${error.message}`);
-        reject(error);
       });
     });
   }
@@ -24,41 +29,36 @@ class RedisClient {
   }
 
   async get(key) {
-    await this.connectPromise; // Wait until the client is connected
+    await this.ensureConnection; // Ensure connection is established
     return new Promise((resolve, reject) => {
       this.client.get(key, (error, result) => {
-        if (error) {
-          return reject(error);
-        }
+        if (error) return reject(error);
         resolve(result);
       });
     });
   }
 
   async set(key, value, duration) {
-    await this.connectPromise; // Wait until the client is connected
+    await this.ensureConnection; // Ensure connection is established
     return new Promise((resolve, reject) => {
       this.client.setex(key, duration, value, (error) => {
-        if (error) {
-          return reject(error);
-        }
+        if (error) return reject(error);
         resolve();
       });
     });
   }
 
   async del(key) {
-    await this.connectPromise; // Wait until the client is connected
+    await this.ensureConnection; // Ensure connection is established
     return new Promise((resolve, reject) => {
       this.client.del(key, (error) => {
-        if (error) {
-          return reject(error);
-        }
+        if (error) return reject(error);
         resolve();
       });
     });
   }
 }
 
+// Create and export an instance of RedisClient
 const redisClient = new RedisClient();
 module.exports = redisClient;
